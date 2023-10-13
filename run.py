c39147e24f74ff1081b644e854a6f5422a1fef03
julia Wager <julia@p-wagner.at>
2023-10-13 13:04:12 +0200

    add entered data to worksheet

--- run.py
+++ run.py
@@ -46,4 +46,15 @@ def validate_data(values):
         return False
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet
+    """
+    print("Updating sales worksheet...\n")
+    sales_workheet = SHEET.worksheet("sales")
+    sales_workheet.append_row(data)
+    print("Sales worksheet updated successfully\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)

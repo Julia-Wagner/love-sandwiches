f21c7a3073057c7f05a0a7f5644ad559cde6c1d7
julia Wager <julia@p-wagner.at>
2023-10-14 12:51:19 +0200

    update surplus data

--- run.py
+++ run.py
@@ -56,11 +56,26 @@ def update_sales_worksheet(data):
     sales_workheet.append_row(data)
     print("Sales worksheet updated successfully\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_workheet = SHEET.worksheet("surplus")
+    surplus_workheet.append_row(data)
+    print("Surplus worksheet updated successfully\n")
+
 def calculate_surplus_data(sales_row):
     print("Calculating surplus data...")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 def main():
     """
@@ -69,7 +84,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
